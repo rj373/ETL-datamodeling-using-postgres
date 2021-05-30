ae501e43136d07c19a9d33076523457913ea791f
Rakesh Jain <45148828+rj373@users.noreply.github.com>
2021-05-30 13:32:05 -0400

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -23,8 +23,8 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time_table (start_time timest
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songplay (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-                        VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)""")
+songplay_table_insert = ("""INSERT INTO songplay (start_time, user_id, level, song_id, artistid, session_id, location, user_agent)
+                        VALUES(%s,%s,%s,%s,%s,%s,%s,%s)""")
 
 user_table_insert = ("""INSERT INTO users (user_id, first_Name, last_Name, gender, level)
                         VALUES(%s,%s,%s,%s,%s) ON CONFLICT (user_id) DO UPDATE SET level = excluded.level""")
